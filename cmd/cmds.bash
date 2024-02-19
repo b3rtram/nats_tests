2cd64fe54daceaf7ad73ff7745054ad5206e639d
Bertram Holzer <bertram@bho-software.de>
2024-02-19 08:20:18 +0100

    add nats server start

--- cmd/cmds.bash
+++ cmd/cmds.bash
@@ -1,3 +1,68 @@
+nats-server -c ./configs/leafnode.cfg                                                                            130 ↵ beholzer@MBPvonBertram3
+# [13504] 2024/02/18 14:37:56.472243 [INF] Starting nats-server
+# [13504] 2024/02/18 14:37:56.472524 [INF]   Version:  2.10.11
+# [13504] 2024/02/18 14:37:56.472528 [INF]   Git:      [not set]
+# [13504] 2024/02/18 14:37:56.472530 [INF]   Cluster:  nats-leaf-node
+# [13504] 2024/02/18 14:37:56.472532 [INF]   Name:     nats-leaf-node
+# [13504] 2024/02/18 14:37:56.472535 [INF]   Node:     R4AyAq5e
+# [13504] 2024/02/18 14:37:56.472537 [INF]   ID:       NBFYWE32NKTAW46K4KYKTVRZRDZHBKPQY6FWZ32GHMBPD6D3D4BDY5J5
+# [13504] 2024/02/18 14:37:56.472548 [INF] Using configuration file: ./configs/leafnode.cfg
+# [13504] 2024/02/18 14:37:56.472830 [INF] Starting JetStream
+# [13504] 2024/02/18 14:37:56.473321 [INF]     _ ___ _____ ___ _____ ___ ___   _   __  __
+# [13504] 2024/02/18 14:37:56.473329 [INF]  _ | | __|_   _/ __|_   _| _ \ __| /_\ |  \/  |
+# [13504] 2024/02/18 14:37:56.473331 [INF] | || | _|  | | \__ \ | | |   / _| / _ \| |\/| |
+# [13504] 2024/02/18 14:37:56.473333 [INF]  \__/|___| |_| |___/ |_| |_|_\___/_/ \_\_|  |_|
+# [13504] 2024/02/18 14:37:56.473337 [INF] 
+# [13504] 2024/02/18 14:37:56.473339 [INF]          https://docs.nats.io/jetstream
+# [13504] 2024/02/18 14:37:56.473341 [INF] 
+# [13504] 2024/02/18 14:37:56.473343 [INF] ---------------- JETSTREAM ----------------
+# [13504] 2024/02/18 14:37:56.473349 [INF]   Max Memory:      12.00 GB
+# [13504] 2024/02/18 14:37:56.473351 [INF]   Max Storage:     212.67 GB
+# [13504] 2024/02/18 14:37:56.473353 [INF]   Store Directory: "data/leaf/store/jetstream"
+# [13504] 2024/02/18 14:37:56.473355 [INF]   Domain:          leafnode
+# [13504] 2024/02/18 14:37:56.473357 [INF] -------------------------------------------
+# [13504] 2024/02/18 14:37:56.474263 [INF]   Starting restore for stream '$G > OBJ_dms'
+# [13504] 2024/02/18 14:37:56.476397 [INF]   Restored 31,860 messages for stream '$G > OBJ_dms' in 2ms
+# [13504] 2024/02/18 14:37:56.476644 [INF] Listening for client connections on 127.0.0.1:6222
+# [13504] 2024/02/18 14:37:56.476656 [INF] Server is ready
+# [13504] 2024/02/18 14:37:56.478132 [INF] 127.0.0.1:7422 - lid:8 - Leafnode connection created for account: $G 
+# [13504] 2024/02/18 14:37:56.481791 [INF] 127.0.0.1:7422 - lid:8 - JetStream using domains: local "leafnode", remote "cluster"
+
+
+nats-server -c ./configs/cluster.cfg                                                                                   beholzer@MBPvonBertram3
+# [13047] 2024/02/18 14:36:44.489092 [INF] Starting nats-server
+# [13047] 2024/02/18 14:36:44.491337 [INF]   Version:  2.10.11
+# [13047] 2024/02/18 14:36:44.491339 [INF]   Git:      [not set]
+# [13047] 2024/02/18 14:36:44.491341 [INF]   Name:     nats-cluster-1
+# [13047] 2024/02/18 14:36:44.491343 [INF]   Node:     FFfB0Dc4
+# [13047] 2024/02/18 14:36:44.491344 [INF]   ID:       NADQATIGQ4MD4MPWYOZHR2PMHNFQHOR6XX6XTTHHFULSBPN3CLOHAAP6
+# [13047] 2024/02/18 14:36:44.491368 [INF] Using configuration file: ./configs/cluster.cfg
+# [13047] 2024/02/18 14:36:44.492724 [INF] Starting JetStream
+# [13047] 2024/02/18 14:36:44.493034 [INF]     _ ___ _____ ___ _____ ___ ___   _   __  __
+# [13047] 2024/02/18 14:36:44.493038 [INF]  _ | | __|_   _/ __|_   _| _ \ __| /_\ |  \/  |
+# [13047] 2024/02/18 14:36:44.493039 [INF] | || | _|  | | \__ \ | | |   / _| / _ \| |\/| |
+# [13047] 2024/02/18 14:36:44.493040 [INF]  \__/|___| |_| |___/ |_| |_|_\___/_/ \_\_|  |_|
+# [13047] 2024/02/18 14:36:44.493057 [INF] 
+# [13047] 2024/02/18 14:36:44.493058 [INF]          https://docs.nats.io/jetstream
+# [13047] 2024/02/18 14:36:44.493060 [INF] 
+# [13047] 2024/02/18 14:36:44.493061 [INF] ---------------- JETSTREAM ----------------
+# [13047] 2024/02/18 14:36:44.493063 [INF]   Max Memory:      12.00 GB
+# [13047] 2024/02/18 14:36:44.493065 [INF]   Max Storage:     212.62 GB
+# [13047] 2024/02/18 14:36:44.493066 [INF]   Store Directory: "data/cluster/store/jetstream"
+# [13047] 2024/02/18 14:36:44.493067 [INF]   Domain:          cluster
+# [13047] 2024/02/18 14:36:44.493068 [INF] -------------------------------------------
+# [13047] 2024/02/18 14:36:44.494521 [INF]   Starting restore for stream '$G > OBJ_DMS_MIRROR'
+# [13047] 2024/02/18 14:36:44.496575 [INF]   Restored 269 messages for stream '$G > OBJ_DMS_MIRROR' in 2ms
+# [13047] 2024/02/18 14:36:44.496957 [INF]   Starting restore for stream '$G > OBJ_DMS_MIRROR2'
+# [13047] 2024/02/18 14:36:44.497678 [INF]   Restored 269 messages for stream '$G > OBJ_DMS_MIRROR2' in 1ms
+# [13047] 2024/02/18 14:36:44.498265 [INF] Listening for leafnode connections on 0.0.0.0:7422
+# [13047] 2024/02/18 14:36:44.498874 [INF] Listening for client connections on 127.0.0.1:4222
+# [13047] 2024/02/18 14:36:44.498883 [INF] Server is ready
+# [13047] 2024/02/18 14:37:56.478437 [INF] 127.0.0.1:55154 - lid:21 - Leafnode connection created 
+# [13047] 2024/02/18 14:37:56.481299 [INF] 127.0.0.1:55154 - lid:21 - JetStream using domains: local "cluster", remote "leafnode"
+
+
+
 nats --server=localhost:6222 object ls
 # ╭───────────────────────────────────────────────────────────────────╮
 # │                        Object Store Buckets                       │
